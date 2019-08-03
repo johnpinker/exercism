ca3e82bd586f0e725fc3d500a8611171168ffda1
John Pinkerton <johnpinker@yahoo.com>
2019-08-03 10:12:13 -0500

    pangram

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        foreach (var c in input) 
+        {
+            if (dict.ContainsKey(c) && char.IsLetter(c))
+                dict[char.ToUpper(c)]++;
+            else if (char.IsLetter(c))
+                dict[char.ToUpper(c)] = 0;
+        }
+        return dict.Select(s => s).Count() == 26 ? true : false;
+    }
+}
