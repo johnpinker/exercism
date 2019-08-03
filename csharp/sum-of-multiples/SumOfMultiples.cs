e1a68e1cca44a10c69867d77b52982975c565145
John Pinkerton <johnpinker@yahoo.com>
2019-08-02 20:45:22 -0500

    sum of multiples

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        return Enumerable.Range(1, max-1).Where(s => {
+            foreach (var v in multiples) 
+                if (v !=0 && s%v == 0) return true;
+            return false;
+        }).Sum();
+        
+    }
+}
