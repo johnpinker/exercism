e8054708437731c4fab99146c129f56135774515
John Pinkerton <johnpinker@yahoo.com>
2019-08-03 15:45:04 -0500

    perfect numbers

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        int sumVal = 0;
+        if (number <= 0)
+            throw new ArgumentOutOfRangeException();
+        sumVal = Enumerable.Range(1, number-1).Where(s => number%s == 0).Sum();
+        switch (sumVal.CompareTo(number))
+        {
+            case 0: 
+                return Classification.Perfect;
+            case 1:
+                return Classification.Abundant;
+            case -1:
+                return Classification.Deficient;
+            default:
+                throw new ArgumentException();
+        }
+    }
+}
