d7eda273db343274d2e617066a3eeb581a7bb3e0
John Pinkerton <johnpinker@yahoo.com>
2019-08-02 22:23:34 -0500

    CollatzConjecture

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number <= 0) throw new ArgumentException();
+        else if (number == 1) return 0;
+        else return (number % 2 == 0 ? Steps(number/2) : Steps(number*3+1))+1;
+    }
+}
