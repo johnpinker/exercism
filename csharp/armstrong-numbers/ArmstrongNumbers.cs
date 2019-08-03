6df200549b101781dfb6090e9ece47ee44e54bac
John Pinkerton <johnpinker@yahoo.com>
2019-08-02 22:50:59 -0500

    armstrong numbers

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        string s = number.ToString();
+        int retVal = 0;
+        for (int i =0; i < s.Length; i++) 
+        {
+            retVal += (int)Math.Pow(Char.GetNumericValue(s[i]), s.Length);
+        }
+        return retVal == number;
+    }
+}
