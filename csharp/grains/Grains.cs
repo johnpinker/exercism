1c75890fcb4e112b812e4bb11d0f22452cfd5c64
John Pinkerton <johnpinker@yahoo.com>
2019-08-02 23:37:11 -0500

    grains

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n < 1 || n > 64)
+            throw new ArgumentOutOfRangeException();
+        if (n==1) return 1;
+        else return Square(n-1)*2;
+    }
+
+    public static ulong Total()
+    {
+        ulong tmpResult = 0;
+        for (int i=1; i<65; i++)
+            tmpResult += Square(i);
+        return tmpResult;
+    }
+}
