c8c4e8b06b9b5612609114de5e3d9d9333333bf4
John Pinkerton <johnpinker@yahoo.com>
2019-08-04 17:02:29 -0500

    isogram

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        string cleanString = new String(word.Where(s => Char.IsLetter(s)).ToArray());
+        string cleanString2 = new String(cleanString.Select(s => Char.ToLower(s)).ToArray());
+        return cleanString2.Distinct().Count() == cleanString2.Length;
+    }
+}
