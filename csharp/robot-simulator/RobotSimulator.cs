d9bfe0465d2b315dd1969c219bb0e73008952310
John Pinkerton <johnpinker@yahoo.com>
2019-08-04 16:24:42 -0500

    robot simulator

--- csharp/robot-simulator/RobotSimulator.cs
+++ csharp/robot-simulator/RobotSimulator.cs
@@ -12,34 +12,60 @@ public class RobotSimulator
 {
     public RobotSimulator(Direction direction, int x, int y)
     {
+        Direction = direction;
+        X = x;
+        Y = y;
     }
 
     public Direction Direction
     {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+        get; private set;
     }
+
 
     public int X
     {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+        get; private set;
     }
 
     public int Y
     {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+        get; private set;
     }
 
     public void Move(string instructions)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        foreach (char c in instructions)
+        {
+            switch (c) 
+            {
+            case 'R' : ChangeDirection(true); break;
+            case 'L' : ChangeDirection(false); break;
+            case 'A' : this.Advance(); break;
+            default: break;
+            }
+        }
     }
+
+    private void ChangeDirection(bool inc)
+    {
+        switch (this.Direction)
+        {
+            case Direction.East: this.Direction = inc ? Direction.South: Direction.North; break;
+            case Direction.South: this.Direction = inc ? Direction.West: Direction.East; break;
+            case Direction.West: this.Direction = inc ? Direction.North: Direction.South; break;
+            case Direction.North: this.Direction = inc ? Direction.East: Direction.West; break;
+        }
+    }
+    private void Advance()
+    {
+        switch (this.Direction)
+        {
+            case Direction.North: this.Y++; break;
+            case Direction.South: this.Y--; break;
+            case Direction.West: this.X--; break;
+            case Direction.East: this.X++; break;
+        }
+    }
+
 }
